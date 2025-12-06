872cdb0d4e9b0dc305d749641101f2c234224a96
mdmuhibullahhabib <mdmuhibullahhabib6348@gmail.com>
2025-12-06 11:45:04 +0600

    home

--- app/page.js
+++ app/page.js
@@ -3,19 +3,14 @@
 import Archive from "@/components/home/Archive";
 import CoursesSection from "@/components/home/CoursesSection";
 import Hero from "@/components/home/Hero";
-import Footer from "@/components/shared/Footer";
-import Navbar from "@/components/shared/Navbar";
 
 export default function Home() {
   return (
     <div>
-      <Navbar/>
       <Hero/>
-      <h1 className="text-red-500">this is home</h1>
       <CoursesSection/>
       <Archive/>
-      <Footer/>
     </div>
-
+    
   );
 }
